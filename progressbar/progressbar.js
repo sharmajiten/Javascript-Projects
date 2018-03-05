dfb688681d24da58bba63c331dd5749f7e5bcbcf
Jiten Sharma <sharmajiten12@gmail.com>
2018-03-04 17:15:11 -0800

    HobbySlider/ProgressBar
    Started on 2nd component of sharmajiten.com , hobbyslider which will slide out as user scrolls

--- progressbar/progressbar.js
+++ progressbar/progressbar.js
@@ -1,1 +1,19 @@
 alert('hello')
+
+// As the page loads, the bar should start depicting "loading Bar". 
+//As it loads , use setInterval function to call the function that targets innerbar div.
+//As the function is called, the bar would show progression
+
+function move() {
+    var elem = document.getElementById("innerbar"); 
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++; 
+            elem.style.width = width + '%'; 
+        }
+    }
+}
